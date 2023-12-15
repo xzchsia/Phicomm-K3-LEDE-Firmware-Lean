85eff73bfae48c87dc8d61322372fce2902b19b8
JE668 <63463703+JE668@users.noreply.github.com>
2023-12-15 18:09:19 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (c) 2020-2024 JE <https://gihub.com/JE668>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/JE668/Phicomm-K3-LEDE-Firmware-Lean
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo '添加Haiibo软件源'
+sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+echo '=========Add Haiibo source OK!========='
+
+# echo '添加my-dnshelper'
+# rm -rf package/lean/luci-app-my-dnshelper
+# git clone https://github.com/kongfl888/openwrt-my-dnshelper/ package/lean/luci-app-my-dnshelper
+# echo '=========Add my-dnshelper OK!========='
+
+# echo '添加adbyby-plus-lite'
+# rm -rf package/lean/luci-app-adbyby-plus
+# git clone https://github.com/kongfl888/luci-app-adbyby-plus-lite package/lean/luci-app-adbyby-plus
+# echo '=========Add adbyby-plus-lite OK!========='
+
+# echo '添加OpenClash'
+# rm -rf package/lean/luci-app-openclash
+# svn checkout https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/lean/luci-app-openclash
+# echo '=========Add OpenClash source OK!========='
+
+echo '添加lwz322的K3屏幕插件'
+rm -rf package/lean/luci-app-k3screenctrl
+git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+echo '=========Add k3screen plug OK!========='
+
+echo '替换lwz322的K3屏幕驱动插件'
+rm -rf package/lean/k3screenctrl
+git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
+echo '=========Replace k3screen drive plug OK!========='
+
+echo '移除bcm53xx中的其他机型'
+sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+# sed -n '140,146p' target/linux/bcm53xx/image/Makefile
+echo '=========Remove other devices of bcm53xx OK!========='
+
+#1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
+firmware='69027'
+echo '替换无线驱动'
+wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+echo '=========Replace k3 wireless firmware OK!========='

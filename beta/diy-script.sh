ccfa500ae91485b21907983797e26df3d00ab8e6
JE668 <63463703+JE668@users.noreply.github.com>
2023-12-17 15:27:23 +0800

    Create diy-script.sh

---
+++ beta/diy-script.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# Copyright (c) 2020-2024 JE <https://gihub.com/JE668>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/JE668/Phicomm-K3-LEDE-Firmware-Lean
+# File name: beta/diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+echo '添加Haiibo软件源'
+sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+echo '=========Add Haiibo source OK!========='
+
+echo '添加AdguardHome'
+rm -rf feeds/haiibo/adguardhome
+rm -rf feeds/haiibo/luci-app-adguardhome
+rm -rf package/feeds/haiibo/luci-app-adguardhome
+rm -rf package/feeds/haiibo/adguardhome
+rm -rf package/lean/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
+echo '=========Add AdguardHome OK!========='
+
+echo '添加lwz322的K3屏幕插件'
+rm -rf package/lean/luci-app-k3screenctrl
+git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+echo '=========Add k3screen plug OK!========='
+
+echo '替换lwz322的K3屏幕驱动插件'
+rm -rf package/lean/k3screenctrl
+git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
+echo '=========Replace k3screen drive plug OK!========='
+
+echo '移除bcm53xx中的其他机型'
+sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+# sed -n '140,146p' target/linux/bcm53xx/image/Makefile
+echo '=========Remove other devices of bcm53xx OK!========='
+
+#1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
+firmware='69027'
+echo '替换无线驱动'
+wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+echo '=========Replace k3 wireless firmware OK!========='
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+echo 'Modify default IP'
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+echo '修改主机名'
+sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
+
+echo '移除主页跑分信息显示'
+sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
+echo '=========Remove benchmark display in index OK!========='
+
+echo '移除主页日志打印'
+sed -i '/console.log(mainNodeName);/d' package/lean/luci-theme-argon/htdocs/luci-static/argon/js/script.js
+echo '=========Remove log print in index OK!========='
+
+echo '修改upnp绑定文件位置'
+sed -i 's/\/var\/upnp.leases/\/tmp\/upnp.leases/g' feeds/packages/net/miniupnpd/files/upnpd.config
+cat feeds/packages/net/miniupnpd/files/upnpd.config |grep upnp_lease_file
+echo '=========Alert upnp binding file directory!========='
+
+添加主页的CPU温度显示
+sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
+echo "Add CPU Temperature in Admin Index OK====================="

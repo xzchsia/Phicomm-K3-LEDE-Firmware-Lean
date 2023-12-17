acda7bde4a653dfd984089d517123864ef976e83
JE668 <63463703+JE668@users.noreply.github.com>
2023-12-17 09:14:57 +0800

    Update diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/JE668/Phicomm-K3-LEDE-Firmware-Lean
-# File name: diy-part1.sh
+# File name: beta/diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 

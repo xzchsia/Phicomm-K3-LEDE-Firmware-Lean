bf28aac4ed38e2a0dabad8610c0971a0d9d24c39
JE668 <63463703+JE668@users.noreply.github.com>
2024-04-10 09:24:09 +0800

    Update diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -13,31 +13,46 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+echo '添加AdguardHome'
+# rm -rf feeds/kenzo/adguardhome
+# rm -rf feeds/kenzo/luci-app-adguardhome
+# rm -rf package/feeds/kenzo/luci-app-adguardhome
+# rm -rf package/feeds/kenzo/adguardhome
+# rm -rf package/lean/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
+echo '=========Add AdguardHome OK!========='
+
+echo '添加lwz322的K3屏幕插件'
+rm -rf package/lean/luci-app-k3screenctrl
+git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+echo '=========Add k3screen plug OK!========='
+
+echo '替换lwz322的K3屏幕驱动插件'
+rm -rf package/lean/k3screenctrl
+git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
+echo '=========Replace k3screen drive plug OK!========='
+
+echo '添加SSR-Plus软件源'
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo '=========Add SSR-Plus source OK!========='
+
+echo '添加passwall软件源'
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo '=========Add passwall source OK!========='
 
 # echo '添加Haiibo软件源'
 # sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 # echo '=========Add Haiibo source OK!========='
 
-echo '添加kenzok8 openwrt-package'
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo '=========Add kenzok8 source OK!========='
+# echo '添加kenzok8 openwrt-package'
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo '=========Add kenzok8 source OK!========='
 
 # echo '添加kenzok8 small-package'
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # echo '=========Add kenzok8 source OK!========='
-
-echo '添加AdguardHome'
-rm -rf feeds/kenzo/adguardhome
-rm -rf feeds/kenzo/luci-app-adguardhome
-rm -rf package/feeds/kenzo/luci-app-adguardhome
-rm -rf package/feeds/kenzo/adguardhome
-rm -rf package/lean/luci-app-adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome package/lean/luci-app-adguardhome 
-echo '=========Add AdguardHome OK!========='
 
 # echo '添加MosDNS'
 # rm -rf feeds/packages/net/v2ray-geodata
@@ -50,16 +65,6 @@ echo '=========Add AdguardHome OK!========='
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # echo '=========Add MosDNS OK!========='
-
-echo '添加lwz322的K3屏幕插件'
-rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/JE668/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
-echo '=========Add k3screen plug OK!========='
-
-echo '替换lwz322的K3屏幕驱动插件'
-rm -rf package/lean/k3screenctrl
-git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
-echo '=========Replace k3screen drive plug OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile

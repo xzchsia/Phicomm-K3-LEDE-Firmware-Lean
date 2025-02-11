f736f275a6e350517c29525cc658a8d049f64b6f
xzchsia <xzchsia@outlook.com>
2025-02-11 04:42:46 +0000

    编译依旧报错，继续尝试修改，路径前添加./

--- diy-part1.sh
+++ diy-part1.sh
@@ -83,8 +83,8 @@ echo '=========Add OpenClash source OK!========='
 echo '移除bcm53xx中的其他机型'
 # sed -i '421,453d' target/linux/bcm53xx/image/Makefile
 # sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' ./target/linux/bcm53xx/image/Makefile
+sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' ./target/linux/bcm53xx/image/Makefile
 # sed -n '140,146p' target/linux/bcm53xx/image/Makefile
 echo '=========Remove other devices of bcm53xx OK!========='
 
@@ -97,5 +97,5 @@ echo '替换无线驱动'
 # mkdir -p package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/
 # wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 rm -rf package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
-wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
+wget -nv https://github.com/JE668/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O ./package/lean/k3-firmware/files/brcmfmac4366c-pcie.bin
 echo '=========Replace k3 wireless firmware OK!========='

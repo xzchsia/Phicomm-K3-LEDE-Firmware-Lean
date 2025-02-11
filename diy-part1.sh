6f02ddec17410df1bce073d05be1b37ff2430f3b
xzchsia <xzchsia@outlook.com>
2025-02-11 04:11:02 +0000

    编译选项里面把除k3以外的机型注释

--- diy-part1.sh
+++ diy-part1.sh
@@ -81,8 +81,9 @@ echo '=========Add OpenClash source OK!========='
 # echo '=========Add MosDNS OK!========='
 
 echo '移除bcm53xx中的其他机型'
-sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+# sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+# sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
 # sed -n '140,146p' target/linux/bcm53xx/image/Makefile
 echo '=========Remove other devices of bcm53xx OK!========='
